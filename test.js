25769ab6bccc111150ff4ebe55c8b55aeffde57d
Paul Iannazzo <somethingitalian@gmail.com>
2012-08-24 11:59:24 -0400

    replaced '+' in console logs with ','

--- test.js
+++ test.js
@@ -10,40 +10,35 @@ var item4 =[{a:2}];
 console.log(
     _.isEqual(
 	Set([item1,item2,item3]).list(),
-	[item1,item2,item3])
-	+
+	[item1,item2,item3]),
 	' _.isEqual(Set([item1,item2,item3]).list(),[item1,item2,item3])');
 
 console.log(
     _.isEqual(
 	Set().add(item4).list(),
-	[item4])
-	+
+	[item4]),
 	' _.isEqual(Set().add(item4).list(),[item4])');
+
 console.log(
     _.isEqual(
 	(Set([item2]).add(item1).list()),
-	[item1,item2])
-	+
+	[item1,item2]),
 	' _.isEqual((Set([item2]).add(item1).list()),[item1,item2])');
 
 console.log(
     _.isEqual(
 	Set([item2]).add(item1).remove(item2).list(),
-	[item1])
-	+
+	[item1]),
 	' _.isEqual(Set([item2]).add(item1).remove(item2).list(),[item1])');
 
 console.log(
     _.isEqual(
 	Set([item2]).add(item1).remove(item4).list(),
-	[item1,item2])
-	+
+	[item1,item2]),
 	' _.isEqual(Set([item2]).add(item1).remove(item4).list(),[item1,item2])');
 
 console.log(
     _.isEqual(
 	Set().remove(item1).remove(item4).list(),
-	[])
-	+
+	[]),
 	' _.isEqual(Set().remove(item1).remove(item4).list(),[])');
