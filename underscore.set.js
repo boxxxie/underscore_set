b605d12e2262fc05840c179abb07033d1b4b6b28
OpenShift guest <somethingitalian@gmail.com>
2012-11-26 01:29:20 -0500

    added proper tabs

--- underscore.set.js
+++ underscore.set.js
@@ -5,51 +5,51 @@
  */
 var _ = require('./libraries/underscore/underscore');
 require('./libraries/underscore_extended/underscore_extended');
-function Set(initial_items){
-    if(initial_items && !_.isObject(initial_items)){
-	throw "the set must be given a list of initial items, or empty arguments"
-    }
-    function initialize_items(initial_items){
-	return _.chain(initial_items || [])
-	.map(JSON.stringify) //make hashes
-	.zip(initial_items) //pair hashes with items
-	.unique(false,_.first) //not sorted
-	.reduce(function(list,item_hash_pair){
-		    var hash = item_hash_pair[0];
-		    var item = item_hash_pair[1];
-		    list[hash] = item;
-		    return list;
-		},{})
-	.value();
+
+function Set(initial_items) {
+    if (initial_items && !_.isObject(initial_items)) {
+        throw "the set must be given a list of initial items, or empty arguments"
     }
 
-    return (function Trusted_Set(set_items, passed_values){
-	   var items = set_items;
-	   return {
-	       add:function(item){
-		   var item_hash = JSON.stringify(item); //only works for sets
-		   var new_set = _.clone(items);
-		   new_set[item_hash] = item;
-		   return Trusted_Set(new_set);
-	       },
-	       remove:function(item){
-		   var item_hash = JSON.stringify(item); //only works for sets
-		   var new_set = _.clone(items);
-		   delete new_set[item_hash];
-		   return Trusted_Set(new_set);
-	       },
-	       exists:function(item){
-		   var item_hash = JSON.stringify(item); //only works for sets
-		   return _.has(items,item_hash);
-	       },
-	       list:function(){
-		   return _.values(items);
-	       },
-	       union:function(set){
-		   return _.fill(items,set);
-	       }
-	   }
-       })
+    function initialize_items(initial_items) {
+        return _.chain(initial_items || []).map(JSON.stringify) //make hashes
+        .zip(initial_items) //pair hashes with items
+        .unique(false, _.first) //not sorted
+        .reduce(function(list, item_hash_pair) {
+            var hash = item_hash_pair[0];
+            var item = item_hash_pair[1];
+            list[hash] = item;
+            return list;
+        }, {}).value();
+    }
+
+    return (function Trusted_Set(set_items, passed_values) {
+        var items = set_items;
+        return {
+            add: function(item) {
+                var item_hash = JSON.stringify(item); //only works for sets
+                var new_set = _.clone(items);
+                new_set[item_hash] = item;
+                return Trusted_Set(new_set);
+            },
+            remove: function(item) {
+                var item_hash = JSON.stringify(item); //only works for sets
+                var new_set = _.clone(items);
+                delete new_set[item_hash];
+                return Trusted_Set(new_set);
+            },
+            exists: function(item) {
+                var item_hash = JSON.stringify(item); //only works for sets
+                return _.has(items, item_hash);
+            },
+            list: function() {
+                return _.values(items);
+            },
+            union: function(set) {
+                return _.fill(items, set);
+            }
+        }
+    })
     (initialize_items(initial_items || []))
 }
 module.exports = Set;
